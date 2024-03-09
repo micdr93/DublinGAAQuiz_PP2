42b418901d98ada9af105ae8062d9701e5cc9180
Michael Drew <michaeldrew_@outlook.com>
2024-03-09 14:18:53 +0000

    added question 4 to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,25 @@
+//Adding quiz QWuestions here
+
 let quizQuestions = [
-   { question: "Whos is the current Dublin Senior Fotball manager?",
-answers: ["Jim Gavin", "Paul Caffrey", "Dessie Farrel", "Pat Gilroy"],
-correctAnswer: "Dessie Farrel"
+   { 
+    question: "Whos is the current Dublin Senior Fotball manager?",
+    answers: ["Jim Gavin", "Paul Caffrey", "Dessie Farrel", "Pat Gilroy"],
+    correctAnswer: "Dessie Farrel"
 },
-   { question: "How many Allstar Awards does Brian Fenton have?",
-        answers: ["Seven", "Five", "Four", "Six"],
-        correctAnswer: "Six"
+   { 
+    question: "How many Allstar Awards does Brian Fenton have?",
+    answers: ["Seven", "Five", "Four", "Six"],
+    correctAnswer: "Six"
     },
-    {question: "Who scored the winning point in the All-Ireland Final versus Kerry?",
+
+    {   
+        question: "Who scored the winning point in the All-Ireland Final versus Kerry?",
         answers: ["Bernard Brogan", "Kevin McManaman", "Stephen Cluxton", "Alan Brogan"],
         correctAnswer: "Stephen Cluxton"
+    },
+    {question: "How many Senior All-Ireland Football titles have Dublin won in total?",
+        answers: ["Twenty-seven", "Thirty-one", "Twenty-nine", "Thirty-three"],
+        correctAnswer: "Thirty-one"
     },
 ];
 
