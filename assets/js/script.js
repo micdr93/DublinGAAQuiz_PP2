719b4bb6435ddbe2365b6aa36a4f79642a0f2168
Michael Drew <michaeldrew_@outlook.com>
2024-03-09 12:31:49 +0000

    added first question in js

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+let quizQuestions = [
+    question: "Whos is the current Dublin Senior Fotball manager?",
+answers: ["Jim Gavin", "Paul Caffrey", "Dessie Farrel", "Pat Gilroy"],
+correctAnswer: "Dessie Farrel"
+},
+
