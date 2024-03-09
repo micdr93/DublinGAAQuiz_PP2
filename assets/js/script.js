c2d263c3bfb0de143c4c887f91a4be125cbbb13b
Michael Drew <michaeldrew_@outlook.com>
2024-03-09 14:04:25 +0000

    added question number two to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 let quizQuestions = [
-    question: "Whos is the current Dublin Senior Fotball manager?",
+   { question: "Whos is the current Dublin Senior Fotball manager?",
 answers: ["Jim Gavin", "Paul Caffrey", "Dessie Farrel", "Pat Gilroy"],
 correctAnswer: "Dessie Farrel"
 },
+   { question: "How man Allstar Awards does Brian Fenton have?",
+        answers: ["7", "5", "4", "6"],
+        correctAnswer: "6"
+    },
+];
 
